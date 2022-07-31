086f39accc4ad1eeb3d95f4c0d38ed53429bce25
josechaparro <josechaparro.dev@gmail.com>
2022-07-31 16:10:06 -0700

    changed the github api url to use the public api

--- js/github.js
+++ js/github.js
@@ -1,11 +1,6 @@
 class Github {
-  constructor() {
-    this.client_id = '57301e21d10995829374';
-    this.client_secret = 'b25bc7d55528e28699897780695dea488652347f';
-  }
-
   async getUser(user) {
-    const response = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const response = await fetch(`https://api.github.com/users/${user}`);
 
     const responseData = await response.json();
 
